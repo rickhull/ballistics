c3a8fb6ce42e283db1c9ab99c33be89b3e728350
Rick Hull <rick.hull@gmail.com>
2017-02-20 01:35:06 -0800

    drop environment; use atmosphere

--- lib/ballistics/core.rb
+++ lib/ballistics/core.rb
@@ -6,15 +6,12 @@ module Ballistics
   1.upto(8) { |g| DRAG_NUM["G#{g}"] = g }
 
   def self.trajectory(opts = {})
-    env = opts[:environment] # optional, possibly nil
-
     opts[:shooting_angle] ||= 0
     opts[:wind_speed] ||= 0
     opts[:wind_angle] ||= 0
     opts[:max_range] ||= 500
     opts[:interval] ||= 1
     opts[:zero_angle] ||= self.zero_angle(opts)
-
 
     args = [
       :drag_function,
@@ -28,8 +25,9 @@ module Ballistics
       :max_range,
       :interval,
     ].map { |param| opts.fetch(param) }
+    atm = opts[:atmosphere]
     args[0] = DRAG_NUM.fetch(args[0])
-    args[1] = env.correct_ballistic_coefficient(args[1]) if env
+    args[1] = atm.translate.(args[1]) if atm
 
     self._map_trajectory(*args)
   end
@@ -48,9 +46,5 @@ module Ballistics
     args[0] = DRAG_NUM.fetch(args[0])
 
     self._calculate_zero_angle(*args)
-  end
-
-  def self.build_environment(options = {})
-    Ballistics::Atmosphere.new(options)
   end
 end
