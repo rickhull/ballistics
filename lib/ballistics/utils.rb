d7fb7ee516d180aef851c4e78d8b2fc1c29f37a8
Rick Hull <rick.hull@gmail.com>
2017-02-20 12:05:02 -0800

    refactor util
    - collapse Ballistics::Util into Ballistics

--- lib/ballistics/utils.rb
+++ lib/ballistics/utils.rb
@@ -2,42 +2,49 @@ require 'bigdecimal'
 require 'bigdecimal/util'
 
 module Ballistics
-  GRAINS = 7000.to_d # per lb
+  # weight in grains
+  # velocity in ft / s
+  # acceleration in ft / s^2
+  # caliber in inches
+
+  GRAINS_PER_LB = 7000.to_d
   G = 32.175.to_d    # gravitational acceleration, feet / s^2
-  COMBO = GRAINS * G
 
-  module Utils
-    def self.sectional_density(weight, diameter)
-      weight.to_d / (GRAINS * diameter.to_d ** 2)
-    end
+  # Sectional density, according to the SpeerReloading Manual No. 13,
+  # is defined as: "A bullet's weight in pounds divided by the square of
+  # its diameter in inches." Note that SD is independent of a bullet's
+  # shape. All bullets of the same caliber and weight will have the same
+  # SD, regardless of their shape or composition.
+  def self.sectional_density(weight, caliber)
+    (weight.to_d / GRAINS_PER_LB) / caliber.to_d ** 2
+  end
 
-    def self.kinetic_energy(velocity, weight)
-      weight.to_d * velocity.to_d ** 2 / (COMBO * 2)
-    end
+  def self.kinetic_energy(velocity, weight)
+    (weight.to_d / GRAINS_PER_LB) * velocity.to_d ** 2 / (G * 2)
+  end
 
-    def self.taylor_ko(velocity, weight, diameter)
-      weight.to_d * velocity.to_d * diameter.to_d / GRAINS
-    end
+  def self.taylor_ko(velocity, weight, caliber)
+    (weight.to_d / GRAINS_PER_LB) * velocity.to_d * caliber.to_d
+  end
 
-    def self.recoil_impulse(bullet_weight, propellant_weight,
-                            velocity, propellant_velocity=4000)
-      (bullet_weight.to_d * velocity.to_d +
-       propellant_weight.to_d * propellant_velocity.to_d) /
-        COMBO
-    end
+  def self.recoil_impulse(bullet_weight, propellant_weight,
+                          velocity, propellant_velocity=4000)
+    (bullet_weight.to_d * velocity.to_d +
+     propellant_weight.to_d * propellant_velocity.to_d) /
+      (GRAINS_PER_LB * G)
+  end
 
-    def self.free_recoil(bullet_weight, propellant_weight,
+  def self.free_recoil(bullet_weight, propellant_weight,
+                       velocity, firearm_weight, propellant_velocity=4000)
+    G * recoil_impulse(bullet_weight, propellant_weight,
+                       velocity, propellant_velocity) /
+      firearm_weight.to_d
+  end
+
+  def self.recoil_energy(bullet_weight, propellant_weight,
                          velocity, firearm_weight, propellant_velocity=4000)
-      G * recoil_impulse(bullet_weight, propellant_weight,
-                         velocity, propellant_velocity) /
-        firearm_weight.to_d
-    end
-
-    def self.recoil_energy(bullet_weight, propellant_weight,
-                           velocity, firearm_weight, propellant_velocity=4000)
-      fr = free_recoil(bullet_weight, propellant_weight,
-                       velocity, firearm_weight, propellant_velocity)
-      firearm_weight.to_d * fr ** 2 / (2 * G)
-    end
+    fr = free_recoil(bullet_weight, propellant_weight,
+                     velocity, firearm_weight, propellant_velocity)
+    firearm_weight.to_d * fr ** 2 / (G * 2)
   end
 end
--- spec/ballistics/utils_spec.rb
+++ spec/ballistics/utils_spec.rb
@@ -3,31 +3,31 @@ require "ballistics/utils"
 describe Ballistics do
   context 'Bullet Calculations' do
     it 'calculates sectional density' do
-      expect(Ballistics::Utils.sectional_density(230, 0.451).round(3)).to eq 0.162
+      expect(Ballistics.sectional_density(230, 0.451).round(3)).to eq 0.162
     end
   end
 
   context 'Energy Calculations' do
     it 'calculates kinetic energy' do
-      expect(Ballistics::Utils.kinetic_energy(800.0, 230).round(2)).to eq 326.78
+      expect(Ballistics.kinetic_energy(800.0, 230).round(2)).to eq 326.78
     end
 
     it 'calculates Taylor Knockout values' do
-      expect(Ballistics::Utils.taylor_ko(800.0, 230, 0.452).round(0)).to eq 12
+      expect(Ballistics.taylor_ko(800.0, 230, 0.452).round(0)).to eq 12
     end
   end
 
   context 'Recoil Calculations' do
     it 'calculates recoil impulse' do
-      expect(Ballistics::Utils.recoil_impulse(150, 46, 2800).round(2)).to eq 2.68
+      expect(Ballistics.recoil_impulse(150, 46, 2800).round(2)).to eq 2.68
     end
 
     it 'calculates free recoil' do
-      expect(Ballistics::Utils.free_recoil(150, 46, 2800, 8).round(1)).to eq 10.8
+      expect(Ballistics.free_recoil(150, 46, 2800, 8).round(1)).to eq 10.8
     end
 
     it 'calculates recoil energy' do
-      expect(Ballistics::Utils.recoil_energy(150, 46, 2800, 8).round(1)).to eq 14.5
+      expect(Ballistics.recoil_energy(150, 46, 2800, 8).round(1)).to eq 14.5
     end
   end
 end
