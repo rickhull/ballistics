bbfa23f10fa616106a356119a8858ba7e2236b51
Rick Hull <rick.hull@gmail.com>
2017-02-20 04:39:31 -0800

    refactor utils

--- lib/ballistics/utils.rb
+++ lib/ballistics/utils.rb
@@ -2,35 +2,42 @@ require 'bigdecimal'
 require 'bigdecimal/util'
 
 module Ballistics
-  UNIT_CORRECTION_FACTOR = 225400.to_d # (7000 gr./lb. * 32.2 f.p.s. ** 2)
-  ACCELERATION_OF_GRAVITY = 32.2.to_d
+  GRAINS = 7000.to_d # per lb
+  G = 32.175.to_d    # gravitational acceleration, feet / s^2
+  COMBO = GRAINS * G
 
   module Utils
-    def self.sectional_density(bullet_weight, bullet_diameter)
-      return ((bullet_weight.to_d / 7000.to_d) / (bullet_diameter.to_d ** 2.to_d))
+    def self.sectional_density(weight, diameter)
+      weight.to_d / (GRAINS * diameter.to_d ** 2)
     end
 
-    def self.kinetic_energy(velocity, bullet_weight)
-      return (0.5.to_d * bullet_weight.to_d * velocity.to_d ** 2.to_d / 7000.to_d / 32.175.to_d)
+    def self.kinetic_energy(velocity, weight)
+      weight.to_d * velocity.to_d ** 2 / (COMBO * 2)
     end
 
-    def self.taylorko(velocity, bullet_weight, bullet_diameter)
-      return ((bullet_weight.to_d * velocity.to_d * bullet_diameter.to_d) / 7000.to_d)
+    def self.taylorko(velocity, weight, diameter)
+      weight.to_d * velocity.to_d * diameter.to_d / GRAINS
     end
 
-    def self.recoil_impulse(bullet_weight, propellant_weight, velocity, propellant_velocity=4000)
-      return (bullet_weight.to_d * velocity.to_d + propellant_weight.to_d * propellant_velocity.to_d) / UNIT_CORRECTION_FACTOR
+    def self.recoil_impulse(bullet_weight, propellant_weight,
+                            velocity, propellant_velocity=4000)
+      (bullet_weight.to_d * velocity.to_d +
+       propellant_weight.to_d * propellant_velocity.to_d) /
+        COMBO
     end
 
-    def self.free_recoil(bullet_weight, propellant_weight, velocity, firearm_weight, propellant_velocity=4000)
-      recoil_impulse = recoil_impulse(bullet_weight, propellant_weight, velocity, propellant_velocity)
-      return (ACCELERATION_OF_GRAVITY * recoil_impulse.to_d / firearm_weight.to_d)
+    def self.free_recoil(bullet_weight, propellant_weight,
+                         velocity, firearm_weight, propellant_velocity=4000)
+      G * recoil_impulse(bullet_weight, propellant_weight,
+                         velocity, propellant_velocity) /
+        firearm_weight.to_d
     end
 
-    def self.recoil_energy(bullet_weight, propellant_weight, velocity, firearm_weight, propellant_velocity=4000)
-      free_recoil = free_recoil(bullet_weight, propellant_weight, velocity, firearm_weight, propellant_velocity)
-      return (firearm_weight.to_d * free_recoil.to_d ** 2.to_d / (ACCELERATION_OF_GRAVITY * 2.to_d))
+    def self.recoil_energy(bullet_weight, propellant_weight,
+                           velocity, firearm_weight, propellant_velocity=4000)
+      fr = free_recoil(bullet_weight, propellant_weight,
+                       velocity, firearm_weight, propellant_velocity)
+      firearm_weight.to_d * fr ** 2 / (2 * G)
     end
   end
-
 end
