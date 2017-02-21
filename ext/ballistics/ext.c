c96714a38c44e5bff2c7513e09264e0b5422401a
Rick Hull <rick.hull@gmail.com>
2017-02-20 17:18:50 -0800

    80ch reformat

--- ext/ballistics/ext.c
+++ ext/ballistics/ext.c
@@ -1,12 +1,21 @@
 #include <ruby.h>
 #include <gnu_ballistics.h>
 
-VALUE method_zero_angle(VALUE self, VALUE drag_function, VALUE drag_coefficient, VALUE velocity, VALUE sight_height, VALUE zero_range, VALUE y_intercept) {
-  double angle =  ZeroAngle(FIX2INT(drag_function), NUM2DBL(drag_coefficient), NUM2DBL(velocity), NUM2DBL(sight_height), NUM2DBL(zero_range), NUM2DBL(y_intercept));
+VALUE method_zero_angle(VALUE self, VALUE drag_function,
+                        VALUE drag_coefficient, VALUE velocity,
+                        VALUE sight_height, VALUE zero_range,
+                        VALUE y_intercept) {
+  double angle =  ZeroAngle(FIX2INT(drag_function), NUM2DBL(drag_coefficient),
+                            NUM2DBL(velocity), NUM2DBL(sight_height),
+                            NUM2DBL(zero_range), NUM2DBL(y_intercept));
   return rb_float_new(angle);
 }
 
-VALUE method_trajectory(VALUE self, VALUE drag_function, VALUE drag_coefficient, VALUE velocity, VALUE sight_height, VALUE shooting_angle, VALUE zero_angle, VALUE wind_speed, VALUE wind_angle, VALUE max_range, VALUE interval) {
+VALUE method_trajectory(VALUE self, VALUE drag_function,
+                        VALUE drag_coefficient, VALUE velocity,
+                        VALUE sight_height, VALUE shooting_angle,
+                        VALUE zero_angle, VALUE wind_speed, VALUE wind_angle,
+                        VALUE max_range, VALUE interval) {
 
   /* cast ruby variables */
   int    DragFunction = FIX2INT(drag_function);
