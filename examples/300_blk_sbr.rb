44848fd34c3607a351130cf6e7f38423ba5af438
Rick Hull <rick.hull@gmail.com>
2017-09-21 19:46:25 +0000

    improve output

--- examples/300_blk_sbr.rb
+++ examples/300_blk_sbr.rb
@@ -8,10 +8,11 @@ cart_id = "barnes_110_vor_tx"
 gun = Ballistics::Gun.load(gun_family).fetch(gun_id)
 cart = gun.cartridges.fetch(cart_id)
 mv = cart.mv(gun.barrel_length)
+proj = cart.projectile
 
 params = {
-  drag_function: cart.projectile.drag_function,
-  ballistic_coefficient: cart.projectile.bc,
+  drag_function: proj.drag_function,
+  ballistic_coefficient: proj.bc,
   velocity: cart.mv(gun.barrel_length),
   sight_height: gun.sight_height,
   zero_range: gun.zero_range,
@@ -28,15 +29,21 @@ puts
 puts "Cartridge: #{cart.name}"
 puts "========="
 puts "     Case: #{cart.case}"
+cart.muzzle_velocity.keys.sort.each { |bl|
+  puts "MV @ #{bl}:".rjust(10, ' ') + " #{cart.muzzle_velocity[bl]}"
+}
+puts " Muzzle V:"
 puts "     Desc: #{cart.desc}" if cart.desc
 puts
-puts "Projectile: #{cart.projectile.name}"
+puts "Projectile: #{proj.name}"
 puts "=========="
-puts "   Caliber: #{cart.projectile.cal}"
-puts "    Grains: #{cart.projectile.grains}"
-puts "      Desc: #{cart.projectile.desc}" if cart.projectile.desc
-puts "        " +
-     [cart.projectile.drag_function, cart.projectile.bc].join(': ')
+puts "   Caliber: #{proj.cal}"
+puts "    Grains: #{proj.grains}"
+proj.ballistic_coefficient.keys.sort.each { |model|
+  puts "BC (#{model.upcase}):".rjust(11, ' ') +
+       " #{proj.ballistic_coefficient[model]}"
+}
+puts "      Desc: #{proj.desc}" if proj.desc
 puts
 puts
 puts Ballistics.table(params)
