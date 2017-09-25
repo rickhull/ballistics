ffe4e18749d377fcb84e4bfc356e8fa6535ae211
Rick Hull <rick.hull@gmail.com>
2017-09-25 06:48:03 +0000

    use Problem.simple

--- examples/300_blk_sbr.rb
+++ examples/300_blk_sbr.rb
@@ -4,10 +4,6 @@ gun_type = 'rifles' # pistols|shotguns
 gun_id = 'ar15_300_blk'
 cart_id = 'barnes_110_vor_tx'
 
-prob = Ballistics::Problem.new { |b|
-  b.gun = Ballistics::Gun.find('rifles').fetch(gun_id)
-  b.cartridge = b.gun.cartridges.fetch(cart_id)
-  b.projectile = b.cartridge.projectile
-}
+prob = Ballistics::Problem.simple(gun_id: gun_id, cart_id: cart_id)
 
 puts [prob.multiline, Ballistics.table(prob.params)].join("\n\n")
