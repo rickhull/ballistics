1d4babab1eb4af141c57ffb59ef1afc166bce18f
Rick Hull <rick.hull@gmail.com>
2017-09-25 06:54:10 +0000

    use gun_family

--- examples/300_blk_sbr.rb
+++ examples/300_blk_sbr.rb
@@ -1,9 +1,7 @@
 require 'ballistics/problem'
 
-gun_type = 'rifles' # pistols|shotguns
-gun_id = 'ar15_300_blk'
-cart_id = 'barnes_110_vor_tx'
-
-prob = Ballistics::Problem.simple(gun_id: gun_id, cart_id: cart_id)
+prob = Ballistics::Problem.simple(gun_family: 'rifles',
+                                  gun_id: 'ar15_300_blk',
+                                  cart_id: 'barnes_110_vor_tx')
 
 puts [prob.multiline, Ballistics.table(prob.params)].join("\n\n")
