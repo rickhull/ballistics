d716baee520b7ed0bc03a1225ad385b23f86ffb3
Rick Hull <rick.hull@gmail.com>
2017-09-25 21:27:39 +0000

    use Ballistics::Problem.table rather than Ballistics.table

--- examples/table.rb
+++ examples/table.rb
@@ -4,4 +4,7 @@ prob = Ballistics::Problem.simple(gun_family: 'rifles',
                                   gun_id: 'ar15_300_blk',
                                   cart_id: 'barnes_110_vor_tx')
 
-puts [prob.multiline, Ballistics.table(prob.params)].join("\n\n")
+puts prob.multiline
+puts
+puts
+puts prob.table
