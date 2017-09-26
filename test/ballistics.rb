b32e1f208a462a4510084f436c5aeff1bbdc98e2
Rick Hull <rick.hull@gmail.com>
2017-09-26 06:14:04 +0000

    add basic trajectory test

--- test/ballistics.rb
+++ test/ballistics.rb
@@ -12,4 +12,20 @@ describe Ballistics do
     }
     Ballistics.zero_angle(opts).round(6).must_equal 0.227188
   end
+
+  it "calculates the expected trajectory" do
+    opts = {
+      drag_number: 1,
+      ballistic_coefficient: 0.5,
+      velocity: 2250,
+      sight_height: 2.6,
+      zero_range: 50,
+      wind_speed: 10,
+      wind_angle: 90,
+      max_range: 1000,
+      interval: 25,
+      shooting_angle: 0,
+    }
+    Ballistics.trajectory(opts).must_be_kind_of Array
+  end
 end
