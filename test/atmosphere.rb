14baa1793214f4da8f3cced1e124708298a78204
Rick Hull <rick.hull@gmail.com>
2017-09-27 17:03:52 +0000

    add test for Atmosphere.translate

--- test/atmosphere.rb
+++ test/atmosphere.rb
@@ -5,13 +5,22 @@ include Ballistics
 
 describe Atmosphere do
   before do
-    @atm = Atmosphere.new("altitude" => 5430,
-                          "humidity" => 0.48,
-                          "pressure" => 29.93,
-                          "temp" => 40)
+    @altitude = 5430
+    @humidity = 0.48
+    @pressure = 29.93
+    @temp = 40
   end
 
   it "translates a ballistic coefficient" do
-    @atm.translate(0.338).round(3).must_equal 0.392
+    Atmosphere.translate(0.338,
+                         altitude: @altitude,
+                         humidity: @humidity,
+                         pressure: @pressure,
+                         temp: @temp).round(3).must_equal 0.392
+
+    Atmosphere.new("altitude" => @altitude,
+                   "humidity" => @humidity,
+                   "pressure" => @pressure,
+                   "temp" => @temp).translate(0.338).round(3).must_equal 0.392
   end
 end
